5bc74044e5ecf1b442f754911cfe682b336be367
Jeff Dinsmore <hello@jeffdinsmore.com>
2020-08-11 23:26:39 -0700

    updated the two buttons where one is hidden and the other shows with javascript and jquery. I gave the light and dark text strong and em elements and put divs around the buttons for them to hide and show.

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,14 @@ $(document).ready(function(){
  $("button#dark").click(function() { 
  $("body").removeClass();
  $("body").addClass("black-background");
+ $(".dark-show").hide();
+ $(".light-hide").show();
 });
 
 $("button#light").click(function() {
 $("body").removeClass();
 $("body").addClass("white-background");
+$(".light-hide").hide();
+$(".dark-show").show();
 });
 });
